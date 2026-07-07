fe075715d7c7d4c5c4c89a4cd2046fb287253f41
mzervini <mixaliszervinis@gmail.com>
2026-07-07 11:46:04 +0300

    add user and message model, db connection and cors

---
+++ backend/src/models/message.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const messsageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    video: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+const Message = mongoose.Model("Message", messsageSchema);
+
+export default Message;
